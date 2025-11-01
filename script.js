8cfebff398b80359bb261e8ee724435b5c33b0d2
kaito24mako <kaitowatanabemcc@gmail.com>
2025-11-01 21:39:41 +1100

    add logic for changes in player turns, though may not work

--- script.js
+++ script.js
@@ -8,22 +8,30 @@ const player2 = createPlayer("Bey", "O");
 
 // Game logic 
 const gameLogic = (function () {
+     let currentPlayer = player1;
 
     // create 3x3 gameboard
     const gameboardObj = {
         gameboard: ["", "", "", "", "", "", "", "", ""]
     };
 
-    // player's turn 
-    // check whose turn it is 
-
-    // check for win/draw conditions after each move 
+    // logic for every round 
     for (i = 0; i < gameboard.length; i++) {
 
+        // player takes turn 
+        currentPlayer // MOVES THIS TURN 
+        
+        // check for win/draw conditions after each move 
+
+        // change player's turn 
+        if (currentPlayer === player1) {
+            currentPlayer = player2;
+        } else (currentPlayer === player2) {
+            currentPlayer = player1;
+        };
     }
 
     // decide when the game ends 
 
     // play again or reset 
-
-});
+})();
