6686a999f9b0756f49230409220d0372b641777d
kaito24mako <kaitowatanabemcc@gmail.com>
2025-11-02 03:13:47 +1100

    create function for displaying player's move

--- simplified.js
+++ simplified.js
@@ -1,3 +1,5 @@
+/* Variables */
+
 /* Create players */
 
 function Player(name, marker) {
@@ -8,12 +10,25 @@ function Player(name, marker) {
 const player1 = new Player("Kaito", "X");
 const player2 = new Player("Bey", "O");
 
+/* Handler for player's move */
+let currentPlayer = player1;
 
+function displayMove() {
+    const cell = document.querySelectorAll(".cell");
 
-const cell = document.querySelectorAll(".cell");
-cell.forEach(cell => {
-    cell.addEventListener("click", (e) => {
-        e.target.textContent = "X";
-    })
-})
+    cell.forEach(cell => {
+        if (currentPlayer === player1) {
+            cell.addEventListener("click", (e) => {
+                e.target.textContent = "X";
+            });
+        } else {
+            cell.addEventListener("click", (e) => {
+                e.target.textContent = "O";
+            });
+        };
+    });
+};
+
+displayMove();
+
 
