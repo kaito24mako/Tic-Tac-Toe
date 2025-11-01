4056ad7004e883991ffae3175a1379d922a722f8
kaito24mako <kaitowatanabemcc@gmail.com>
2025-11-01 21:08:02 +1100

    put gameboard object into IIFE module

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// 3x3 gameboard
-const gameboard = {
-    gameboard: ["", "", "", "", "", "", "", "", ""]
-};
-
-// Create players
+// Create players 
 function createPlayer(name, mark) {
     // store into an object
     return {name, mark};
@@ -11,4 +6,21 @@ function createPlayer(name, mark) {
 const player1 = createPlayer("Kaito", "X");
 const player2 = createPlayer("Bey", "O");
 
+// Game logic 
+const gameLogic = (function () {
 
+    // create 3x3 gameboard
+    const gameboardObj = {
+        gameboard: ["", "", "", "", "", "", "", "", ""]
+    };
+
+    // select player to go first
+    //! if previous winner won, then they go first
+    //! else other player goes first
+    //! else if no winner, choose at random 
+
+    // declare winner or draw 
+    for (i = 0; i < gameboard.length; i++) {
+
+    }
+})
